3f944ec5ef04a3c1689c36f786fdcd30dd1a3377
Patrick Barreto dos Santos <patrickbarreto@id.uff.br>
2017-06-08 19:02:49 -0300

    Criação das Regras ;
    Constam alguns Comentários sobre os métodos.
    
    Itens:
    validaAluno(String matricula)
    vericaStatus(Aluno aluno)
    sugestaoUm(String nome)
    sugestaoUm(String nome)
    geraSugestoes(String nome)
    
    Obs: Aplicação rodando, gerando duas sugestões de e-mail.

--- src/desafiostimail/DesafioStiMail.java
+++ src/desafiostimail/DesafioStiMail.java
@@ -5,9 +5,12 @@
  */
 package desafiostimail;
 
+import Classes.Aluno;
 import Classes.Mensagem;
+import Classes.Regras;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
@@ -26,6 +29,20 @@ public class DesafioStiMail {
 
             String matricula = JOptionPane.showInputDialog("Digite a sua matrícula:");
             System.out.println(matricula);
+            	
+            Aluno aluno = Regras.validaAluno(matricula);
+            //criar regra para validar status e se o aluno já possui uffmail;
+            //comentar o código;
+            
+            List sugestoes = Regras.geraSugestoes(aluno.getNome());
+            
+            Mensagem.escolha(sugestoes, aluno.getNome());
+            
+            Scanner sc = new Scanner(System.in);
+            int escolha = Integer.parseInt(sc.nextLine());
+            
+            Mensagem.pedidoUffMailGerado((String) sugestoes.get(escolha-1));
+            Mensagem.senhaSMS(aluno.getTelefone());
             
 
     }
