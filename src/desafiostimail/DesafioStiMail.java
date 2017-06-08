14f36fd51bc8e51fc77c8f5c112f66061c28f954
Patrick Barreto dos Santos <patrickbarreto@id.uff.br>
2017-06-08 18:31:19 -0300

    Commit inicial - Criação do Projeto. Itens:
    - Classe principal: DesafioStiMail.java
    *Conteúdo: Caixa de Texto para inputar uma matricula e exibi-la em seguida.
    
    - Classes aluno(atributos + geters e seters), mensagem e regras.
    
    - Arquivo CSV incluso.

---
+++ src/desafiostimail/DesafioStiMail.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desafiostimail;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author psantos
+ */
+public class DesafioStiMail {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+            
+            String matricula = JOptionPane.showInputDialog("Digite a sua matrícula:");
+            System.out.println(matricula);
+            
+
+    }
+}
